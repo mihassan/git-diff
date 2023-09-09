6bdefab9d91135167d6b13cab009dd7d57ef6c54
Md Imrul Hassan <mihassan@gmail.com>
2023-09-09 16:51:27 +1000

    Cleanup ParserSpec by removing redundant type annotations.

--- test/Lib/ParserSpec.hs
+++ test/Lib/ParserSpec.hs
@@ -3,9 +3,7 @@
 
 module Lib.ParserSpec (spec) where
 
-import Data.Text
 import Text.Megaparsec
-import Text.Megaparsec.Char
 import Test.Hspec
 import Test.Hspec.Megaparsec
 import Lib
@@ -14,122 +12,92 @@ spec :: Spec
 spec = do
   describe "chunkP Parser" $ do
     it "should parse valid chunk" $ do
-      parse chunkP "" ("@@ -1,6 +1,6 @@\n Unchanged\n+Added\n-Removed" :: Text)
+      parse chunkP "" "@@ -1,6 +1,6 @@\n Unchanged\n+Added\n-Removed"
         `shouldParse`
           Chunk (LineRange 1 6) (LineRange 1 6) [ContextLine "Unchanged", AddedLine "Added", RemovedLine "Removed"]
 
     it "should parse valid chunk with newline" $ do
-      parse chunkP "" ("@@ -1,6 +1,6 @@\n Unchanged\n+Added\n-Removed\n" :: Text)
+      parse chunkP "" "@@ -1,6 +1,6 @@\n Unchanged\n+Added\n-Removed\n"
         `shouldParse`
           Chunk (LineRange 1 6) (LineRange 1 6) [ContextLine "Unchanged", AddedLine "Added", RemovedLine "Removed"]
 
     it "should parse valid chunk with more non-chunk lines" $ do
-      parse chunkP "" ("@@ -1,6 +1,6 @@\n Unchanged\n+Added\n-Removed\nend of chunk\n" :: Text)
+      parse chunkP "" "@@ -1,6 +1,6 @@\n Unchanged\n+Added\n-Removed\nend of chunk\n"
         `shouldParse`
           Chunk (LineRange 1 6) (LineRange 1 6) [ContextLine "Unchanged", AddedLine "Added", RemovedLine "Removed"]
 
     it "should fail with empty input" $ do
-      parse chunkP "" `shouldFailOn` ("" :: Text)
+      parse chunkP "" `shouldFailOn` ""
 
     it "should fail with invalid chunk header" $ do
-      parse chunkP "" `shouldFailOn` ("-1,6 +1,6\n Unchanged" :: Text)
+      parse chunkP "" `shouldFailOn` "-1,6 +1,6\n Unchanged"
 
     it "should fail with single line range" $ do
-      parse chunkP "" `shouldFailOn` ("@@ -1,6 @@\n Unchanged" :: Text)
+      parse chunkP "" `shouldFailOn` "@@ -1,6 @@\n Unchanged"
 
     it "should fail with more than two line ranges" $ do
-      parse chunkP "" `shouldFailOn` ("@@ -1,6 +1,6 +1,16 @@\n Unchanged" :: Text)
+      parse chunkP "" `shouldFailOn` "@@ -1,6 +1,6 +1,16 @@\n Unchanged"
 
     it "should fail with no chunk header" $ do
-     parse chunkP "" `shouldFailOn` ("+Added\n Unchanged" :: Text)
+     parse chunkP "" `shouldFailOn` "+Added\n Unchanged"
 
     it "should fail with no diff line" $ do
-      parse chunkP "" `shouldFailOn` ("@@ -1,6 +1,6 @@" :: Text)
+      parse chunkP "" `shouldFailOn` "@@ -1,6 +1,6 @@"
 
 
   describe "chunkHeaderP Parser" $ do
     it "should parse valid chunk header" $ do
-      parse chunkHeaderP "" ("@@ -1,6 +1,5 @@\n" :: Text) `shouldParse` [LineRange 1 6, LineRange 1 5]
+      parse chunkHeaderP "" "@@ -1,6 +1,5 @@\n" `shouldParse` [LineRange 1 6, LineRange 1 5]
 
     it "should fail on invalid chunk header" $ do
-      parse chunkHeaderP "" `shouldFailOn` ("-1,6 +1,5\n" :: Text)
+      parse chunkHeaderP "" `shouldFailOn` "-1,6 +1,5\n"
 
 
   describe "lineRangeP Parser" $ do
     it "should parse single line range with minus prefix" $ do
-      parse lineRangeP "" ("-15" :: Text)
-        `shouldParse`
-        LineRange 15 1
+      parse lineRangeP "" "-15" `shouldParse` LineRange 15 1
 
     it "should parse single line range with plus prefix" $ do
-      parse lineRangeP "" ("+15" :: Text)
-        `shouldParse`
-        LineRange 15 1
+      parse lineRangeP "" "+15" `shouldParse` LineRange 15 1
 
     it "should parse multi line range with minus prefix" $ do
-      parse lineRangeP "" ("-15,5" :: Text)
-        `shouldParse`
-        LineRange 15 5
+      parse lineRangeP "" "-15,5" `shouldParse` LineRange 15 5
 
     it "should parse multi line range with plus prefix" $ do
-      parse lineRangeP "" ("+15,5" :: Text)
-        `shouldParse`
-        LineRange 15 5
+      parse lineRangeP "" "+15,5" `shouldParse` LineRange 15 5
 
 
     it "should fail without minus or plus prefix" $ do
-      parse lineRangeP ""
-        `shouldFailOn`
-        ("15,5" :: Text)
+      parse lineRangeP "" `shouldFailOn` "15,5"
 
     it "should fail with letter" $ do
-      parse lineRangeP ""
-        `shouldFailOn`
-        ("range 15,5" :: Text)
+      parse lineRangeP "" `shouldFailOn` "range 15,5"
 
     it "should fail with floating point number" $ do
-      parse lineRangeP ""
-        `shouldFailOn`
-        ("+.5,5" :: Text)
+      parse lineRangeP "" `shouldFailOn` "+.5,5"
 
     it "should fail with space prefix" $ do
-      parse lineRangeP ""
-        `shouldFailOn`
-        (" 15,5" :: Text)
+      parse lineRangeP "" `shouldFailOn` " 15,5"
 
   describe "lineDiffP Parser" $ do
     it "should parse added line" $ do
-      parse lineDiffP "" ("+Added Line" :: Text)
-        `shouldParse`
-        AddedLine "Added Line"
+      parse lineDiffP "" "+Added Line" `shouldParse` AddedLine "Added Line"
 
     it "should parse removed line" $ do
-      parse lineDiffP "" ("-Removed Line" :: Text)
-        `shouldParse`
-        RemovedLine "Removed Line"
+      parse lineDiffP "" "-Removed Line" `shouldParse` RemovedLine "Removed Line"
 
     it "should parse context line" $ do
-      parse lineDiffP "" (" Unchanged Line" :: Text)
-        `shouldParse`
-        ContextLine "Unchanged Line"
+      parse lineDiffP "" " Unchanged Line" `shouldParse` ContextLine "Unchanged Line"
 
 
     it "should fail with line starting with letter" $ do
-      parse lineDiffP ""
-        `shouldFailOn`
-        ("Invalid Line" :: Text)
+      parse lineDiffP "" `shouldFailOn` "Invalid Line"
 
     it "should fail with line starting with symbol" $ do
-      parse lineDiffP ""
-        `shouldFailOn`
-        ("=Invalid Line" :: Text)
+      parse lineDiffP "" `shouldFailOn` "=Invalid Line"
 
     it "should fail with line starting with digit" $ do
-      parse lineDiffP ""
-        `shouldFailOn`
-        ("0Invalid Line" :: Text)
+      parse lineDiffP "" `shouldFailOn` "0Invalid Line"
 
     it "should fail with empty line" $ do
-      parse lineDiffP ""
-        `shouldFailOn`
-        ("" :: Text)
+      parse lineDiffP "" `shouldFailOn` ""
